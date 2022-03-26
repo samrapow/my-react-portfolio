8b7d89fa0d88e316b12c98909b21e10bb2e7dd48
Sam Rapowitz <samrapow@gmail.com>
2022-03-25 20:42:58 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import OmdbContainer from './components/OmdbContainer';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/index";
+import Wrapper from "./components/Wrapper/index";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import ResumePage from "./pages/ResumePage";
 
-export default function App() {
-  return <OmdbContainer />;
+function App() {
+  return (
+    <HashRouter basename="/">
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/resume" component={ResumePage} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
 }
+
+export default App;
